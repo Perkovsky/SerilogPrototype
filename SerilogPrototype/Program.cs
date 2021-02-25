e11b29a67d10e7d2f2ce76dc2ef1b59a12429bc3
Igor Perkovsky <igor.perkovsky@mail.ru>
2021-02-25 14:23:40 +0200

    The 1st version of the prototype has been completed

--- SerilogPrototype/Program.cs
+++ SerilogPrototype/Program.cs
@@ -34,50 +34,10 @@ namespace SerilogPrototype
 
 				var awsSettings = config.GetSection("AWSSettings").Get<AWSSettings>();
 
-				//AmazonS3(string path, string bucketName, RegionEndpoint endpoint, string awsAccessKeyId, string awsSecretAccessKey, bool autoUploadEvents = false, LogEventLevel restrictedToMinimumLevel = LogEventLevel.Verbose, string outputTemplate = "{Timestamp:yyyy-MM-dd HH:mm:ss.fff zzz} [{Level:u3}] {Message:lj}{NewLine}{Exception}", IFormatProvider formatProvider = null, long ? fileSizeLimitBytes = 1073741824, LoggingLevelSwitch levelSwitch = null, bool buffered = false, RollingInterval rollingInterval = RollingInterval.Day, int ? retainedFileCountLimit = 31, Encoding encoding = null, FileLifecycleHooks hooks = null, Action < Exception > failureCallback = null, string bucketPath = null);
-
-				//	target.WriteTo.AmazonS3(
-				//		path: "log.txt",
-				//		bucketName: awsSettings.Bucket,
-				//		endpoint: RegionEndpoint.USEast1,
-				//		awsAccessKeyId: awsSettings.AccessKeyId,
-				//		awsSecretAccessKey: awsSettings.AccessKey,
-				//		restrictedToMinimumLevel: LogEventLevel.Debug,
-				//		outputTemplate: "{Timestamp:o} [{Level:u3}] {Message}{NewLine}{Exception}",
-				//		retainedFileCountLimit: null,
-				//		//fileSizeLimitBytes: 1048576, // 1mb
-				//		//rollingInterval: RollingInterval.Minute,
-				//		//buffered: true,
-				//		autoUploadEvents: true,
-				//		//levelSwitch: levelSwitch,
-				//		//encoding: Encoding.Unicode,
-				//		//hooks: new HeaderWriter("Timestamp,Level,Message"),
-				//		//bucketPath = "awsSubPath",
-				//		failureCallback: e => Console.WriteLine($"An error occured in AmazonS3 sink: {e.Message}")
-				//	);
-
-
 				Log.Logger = new LoggerConfiguration()
 					.ReadFrom.Configuration(config)
-					.WriteTo.Map(DEVICE_ID_PROPERTY_NAME, "_general", (name, wt) =>
+					.WriteTo.Map(DEVICE_ID_PROPERTY_NAME, /*"_general",*/ (name, wt) =>
 					{
-						//wt.File(
-						//	path: $@"{BASE_LOG_FOLDER}\{name}.txt",
-						//	outputTemplate: outputTemplate
-						//);
-						//wt.Elasticsearch(new ElasticsearchSinkOptions(new Uri(elasticsearchEndpoint))
-						//{
-						//	AutoRegisterTemplate = true,
-						//	OverwriteTemplate = true,
-						//	DetectElasticsearchVersion = true,
-						//	IndexFormat = $"mg-{name}-index-{{0:yyyy.MM}}",
-						//	EmitEventFailure = EmitEventFailureHandling.WriteToSelfLog,
-						//	RegisterTemplateFailure = RegisterTemplateRecovery.IndexAnyway
-						//});
-
-						//sb.Append($"{now.Year}/{now.Month:#00}/{now.Day:#00}");
-
-
 						wt.AmazonS3(
 							path: "log.txt",
 							bucketName: awsSettings.BucketURL,
@@ -87,48 +47,30 @@ namespace SerilogPrototype
 							restrictedToMinimumLevel: LogEventLevel.Debug,
 							outputTemplate: "{Timestamp:o} [{Level:u3}] {Message}{NewLine}{Exception}",
 							bucketPath: $"DeviceLog/{name}",
-							//rollingInterval: Serilog.Sinks.AmazonS3.RollingInterval.Minute,
 							batchSizeLimit: int.MaxValue,
 							batchingPeriod: TimeSpan.FromMinutes(5),
 							eagerlyEmitFirstEvent: false,
 							failureCallback: ex => Console.WriteLine($"An error occured in AmazonS3 sink: {ex.Message}")
 						);
 					})
-					//.WriteTo.AmazonS3(
-					//	path: "log.txt",
-					//	bucketName: awsSettings.BucketURL,
-					//	endpoint: RegionEndpoint.USEast1,
-					//	awsAccessKeyId: awsSettings.AccessKeyId,
-					//	awsSecretAccessKey: awsSettings.AccessKey,
-					//	restrictedToMinimumLevel: LogEventLevel.Debug,
-					//	outputTemplate: "{Timestamp:o} [{Level:u3}] {Message}{NewLine}{Exception}",
-					//	//autoUploadEvents: true, // very looong work
-					//	//fileSizeLimitBytes: 5242880, //5mb		// 1048576, // 1mb
-					//	//buffered: true,
-					//	//new CultureInfo("de-DE"),
-					//	//levelSwitch: levelSwitch,
-					//	//retainedFileCountLimit: 10,
-					//	//encoding: Encoding.Unicode,
-					//	//hooks: new HeaderWriter("Timestamp,Level,Message"),
-					//	bucketPath: "DeviceLog/www-44-mmm",
-					//	//rollingInterval: Serilog.Sinks.AmazonS3.RollingInterval.Minute,
-					//	batchSizeLimit: int.MaxValue,
-					//	batchingPeriod: TimeSpan.FromMinutes(3),
-					//	eagerlyEmitFirstEvent: false,
-					//	failureCallback: ex => Console.WriteLine($"An error occured in AmazonS3 sink: {ex.Message}")
-					//)
 					.CreateLogger();
 
 				for (var x = 0; x < 200; x++)
 				{
 					var ex = new Exception($"Test - {x}");
-					Log.Logger.Error($"{{{DEVICE_ID_PROPERTY_NAME}}}. {ex.ToString()}", DEVICE_ID);
+					Log.Logger.Error(ex, $"{{{DEVICE_ID_PROPERTY_NAME}}}", DEVICE_ID);
 				}
 
 				await EmulateWorkAsync();
 
+				// without DeviceId
 				Log.Logger.Information("This is Information without device ID");
 				Log.Logger.Error("This is Error without device ID");
+
+				// DeviceId = "111222777"
+				Log.Logger.Information($"{{{DEVICE_ID_PROPERTY_NAME}}}. This is Information", "111222777");
+				Log.Logger.Debug($"{{{DEVICE_ID_PROPERTY_NAME}}}. This is Debug", "111222777");
+				Log.Logger.Error($"{{{DEVICE_ID_PROPERTY_NAME}}}. This is Error", "111222777");
 
 				Log.Logger.Information($"{{{DEVICE_ID_PROPERTY_NAME}}}. This is Information", DEVICE_ID);
 				Log.Logger.Debug($"{{{DEVICE_ID_PROPERTY_NAME}}}. This is Debug", DEVICE_ID);
